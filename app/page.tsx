41d140eef645cbc489e216058e8ce438b925244e
Ram Prasad <ramaprasad.d@sigmainfo.net>
2024-11-08 16:50:00 +0530

    Next js Code

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-20">
+        <AcmeLogo />
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/3 md:px-20">
+        <h1 className="text-blue-500">I'm blue!</h1>
+        <p className={`${lusitana.className}`}>Lusitana Font Family</p>
+        <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+      <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+      </div>
+      </div>
+    </main>
+  );
+}
